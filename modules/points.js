fb21d2576e488ac293aa95f8008047cf6891569b
Hubert Stoklosa <68256286+hstoklosa@users.noreply.github.com>
2024-02-09 16:50:45 +0000

    Update points.js

--- modules/points.js
+++ modules/points.js
@@ -1,8 +1,10 @@
 const pointsIndicator = document.getElementById('points');
 
 let points = 0;
+pointsIndicator.innerHTML = points;
 
 export const handlePoints = (enemy) => {
+
     if (enemy.radius <= 10) {
         points = points + 15;
     } else if (enemy.radius <= 2) {
@@ -10,5 +12,6 @@ export const handlePoints = (enemy) => {
     } else {
         points = points + 5;
     }
+
     pointsIndicator.innerHTML = points;
 }
